b715dcf400a3445ab0cdca4d426c6bce670510af
Y.W <stewchicken@gmail.com>
2017-12-17 11:04:53 +0100

    update

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -9,7 +9,7 @@ export default class Dashboard extends Component {
   constructor(props) {
     super(props)
     this.state = { accidents: [] }
-    this.grabAccidents = this.grabAccidents.bind(this)
+    //this.grabAccidents = this.grabAccidents.bind(this)
   }
 
   // use observable instead of timer (next step)
